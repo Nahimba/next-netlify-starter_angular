1a5a6af22fd3bb3078a38ffd6687acf7a49e8eb1
Nahimba <zvictz@gmail.com>
2023-06-27 20:09:44 +0100

    Revert " replace to krypton-games"
    This reverts commit cbcda38d292c8e95953bf4514af41a97ec472087.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/krypton-games'),
+      dir: require('path').join(__dirname, './coverage/angular-quickstart'),
       subdir: '.',
       reporters: [
         { type: 'html' },
